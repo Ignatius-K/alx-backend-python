a64b17baef293079d92d185e66474e964273405b
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-10-11 09:18:21 +0300

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Module defines `concat` function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings
+
+    Args:
+        str1: The first string
+        str2: The second string
+
+    Return:
+        The concatenated string
+
+    Example:
+        - f("hello", "world") -> "helloworld"
+    """
+    return str1 + str2
