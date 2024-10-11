38e492e6a96d6bac5c9cb79f82d4de2718af1ae3
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-10-11 08:58:48 +0300

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Module defines `add` function"""
+
+def add(num1: float, num2: float) -> float:
+    """Adds two float numbers
+
+    Args:
+        num1: The first float number
+        num2; The second float number
+
+    Return:
+        The sum
+    """
+    return num1 + num2
